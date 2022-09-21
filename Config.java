a78f1f8b77e16d9667ab6bbd86edc589a061ce33
Ferfloza2003 <111952244+Ferfloza2003@users.noreply.github.com>
2022-09-21 14:27:00 -0500

    Add files via upload

---
+++ Config.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TareasED;
+
+public class Config
+{
+    public static int alto    = 20;
+    public static int ancho     = 75;
+    public static int ciclos    = 20;
+    public static int celvivs0 = 2;  
+    public static int celvivs1 = 3;  
+    public static int celvivs2 = 3; 
+   
+}
